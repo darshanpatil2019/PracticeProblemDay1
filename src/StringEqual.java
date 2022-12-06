816acfdbe9cccd9b96f52ca28ddefe5b0332d985
Darshan Patil <iampatildarsh>
2022-12-06 15:19:30 +0530

    Programme of 2 Strings

---
+++ src/StringEqual.java
@@ -0,0 +1,12 @@
+public class StringEqual {
+    public static void main(String[] args) {
+        String a = "Darshan";
+        String b = "Patil";
+
+        if (a == b) {
+            System.out.println("Strings are Equal");
+        } else {
+            System.out.println("Strings are not Equal");
+        }
+    }
+}
