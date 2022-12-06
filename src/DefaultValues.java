c9fc3a66f252d15727b69b7e1adba3e412dc9aaf
Darshan Patil <iampatildarsh>
2022-12-06 15:18:46 +0530

    Programme of Default Values of Primitive Data Types

---
+++ src/DefaultValues.java
@@ -0,0 +1,24 @@
+public class DefaultValues {
+
+    static int a;
+    static char c;
+    static long l;
+    static short o;
+    static boolean b;
+    static byte d;
+    static double u;
+    static float t;
+
+    public static void main(String[] args) {
+        System.out.println("Default Value of int = " + a);
+        System.out.println("Default Value of char = " + c);
+        System.out.println("Default Value of long = " + l);
+        System.out.println("Default Value of Short = " + o);
+        System.out.println("Default Value of boolean = " + b);
+        System.out.println("Default Value of byte = " + d);
+        System.out.println("Default Value of double = " + u);
+        System.out.println("Default Value of float = " + t);
+
+    }
+}
+
